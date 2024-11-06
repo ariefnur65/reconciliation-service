ea3539f7692348f913c799265b367645ea995dc6
arief.prakoso <ariefnur652010@gmail.com>
2024-11-06 23:47:19 +0700

    Rename transactions to system transaction in detail mismatched transaction model

--- src/main/java/com/axorean/reconciliation_service/model/DetailMismatchedTransaction.java
+++ src/main/java/com/axorean/reconciliation_service/model/DetailMismatchedTransaction.java
@@ -2,6 +2,6 @@ package com.axorean.reconciliation_service.model;
 
 import java.util.List;
 
-public record DetailMismatchedTransaction (List<SystemTransaction> transactions,
+public record DetailMismatchedTransaction (List<SystemTransaction> systemTransactions,
                                            List<BankStatements> bankStatementsList) {
 }
